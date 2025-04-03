290134767694e573586578601e3f1613c2d1791b
lolyay270 <boyeje2@student.op.ac.nz>
2025-04-04 09:21:24 +1300

    build(movie): react-router import and setup

--- movie-listings-application/src/main.tsx
+++ movie-listings-application/src/main.tsx
@@ -3,13 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 export const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
 )
