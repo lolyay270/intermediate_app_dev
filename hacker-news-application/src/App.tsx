41803895d39a0ad795ac9192a1c979d2769f244b
lolyay270 <boyeje2@student.op.ac.nz>
2025-04-29 09:58:42 +1200

    feat(hacker): started fetch changing per route

--- hacker-news-application/src/App.tsx
+++ hacker-news-application/src/App.tsx
@@ -1,13 +1,34 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { useEffect, useState } from 'react';
+import { queryClient } from "./main";
 
 const App = () => {
+  const fetchUrlBase = "https://hacker-news.firebaseio.com/v0/";
+  const [fetchUrl, setFetchUrl] = useState("");
+
   const {
     data: storyData,
   } = useQuery({
     queryKey: ["storyData"],
     queryFn: () =>
-      fetch("https://hacker-news.firebaseio.com/v0/askstories.json?print=pretty").then((res) => res.json()),
+      fetch(fetchUrlBase + fetchUrl).then((res) => res.json()),
   });
+
+  const { mutate: getStoryMutation, data: getStoryData } =
+  useMutation({
+    mutationFn: () =>
+      fetch(fetchUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json()),
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: ["storyData"],
+      }),
+  });
+
 
   if (storyData === null) return <p>No data available</p>;
   if (storyData && !Array.isArray(storyData)) return <p>Loading...</p>;
