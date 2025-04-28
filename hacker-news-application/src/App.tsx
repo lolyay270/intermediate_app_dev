a20b7a314fc2487a2b8bb47545a55a75fc22d402
lolyay270 <boyeje2@student.op.ac.nz>
2025-04-29 09:20:09 +1200

    feat(hacker): basic fetch with little error checking

--- hacker-news-application/src/App.tsx
+++ hacker-news-application/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const {
+    data: storyData,
+  } = useQuery({
+    queryKey: ["storyData"],
+    queryFn: () =>
+      fetch("https://hacker-news.firebaseio.com/v0/askstories.json?print=pretty").then((res) => res.json()),
+  });
+
+  if (storyData === null) return <p>No data available</p>;
+  if (storyData && !Array.isArray(storyData)) return <p>Loading...</p>;
+  if (storyData && storyData.error) return <p>{storyData.error}</p>;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {storyData && storyData.length > 0 && (
+        <p>{storyData.toString()}</p>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
