77261709a610817dd36ea1de1d0bfb20bbd2fec9
lolyay270 <boyeje2@student.op.ac.nz>
2025-06-19 21:01:23 +1200

    feat(hacker): story data shows on screen

--- hacker-news-application/src/Components/Stories.tsx
+++ hacker-news-application/src/Components/Stories.tsx
@@ -12,131 +12,62 @@ interface StoriesProps {
 const Stories: React.FC<StoriesProps> = (props: StoriesProps) => {
   const fetchUrlBase = "https://hacker-news.firebaseio.com/v0";
   const storyInfoUrlExtension = "/item/";   // full url is .../v0/item/[code].json
-  const [fetchUrl, setFetchUrl] = useState<string>("");
-  const [fetchAllArray, setFetchAllArray] = useState<Promise<Response>[]>([]);
-  const [allStoryInfo, setAllStoryInfo] = useState<any[]>([]);
+  let fetchUrl: string = "";
 
-
-
-
-  
-  //get the codes for all stories
-  let { data: storiesIds } = useQuery({
-    queryKey: ["storyIds"],
-    queryFn: () => fetch(fetchUrl).then((res) => res.json()),
+  //set the fetch url based on the page were loading
+  navItems.forEach(nav => {
+    if (nav.localUrl === props.localUrl) {
+      fetchUrl = fetchUrlBase + nav.fetchUrl;
+    }
   });
 
-  const { mutate: getStoriesIdsMutation, data: getStoriesIdsData } = useMutation({
-    mutationFn: () =>
-      fetch(fetchUrl, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }).then((res) => {
-        storiesIds = getStoriesIdsData;
-        res.json();
-      }),
-    onSuccess: () =>
-      queryClient.invalidateQueries({
-        queryKey: ["storyIds"],
-      }),
-  });
+  // Get story IDs
+ const {
+   data: storiesIds,
+   isLoading: isLoadingIds,
+   isError: isErrorIds,
+ } = useQuery({
+   queryKey: ["storyIds", fetchUrl],
+   queryFn: () => fetch(fetchUrl).then((res) => res.json()),
+ });
 
-  //get the data for each story
-  const { mutate: getStoryInfoMutation, data: getStoryInfoData, status: getStoryInfoStatus, error: getStoryInfoError} = useMutation({
-    mutationFn: () => 
-      Promise.all(fetchAllArray).then((resArray) => {
-        let storyDatas: any[] = [];
-        
-        resArray.map(async(res) => {
-          const story: any = await res.json();
-          storyDatas.push(story)
-
-          // try{
-          //   const newStory: StoryProps = {
-          //     by: story.by,
-          //     descendants: story.descendants,
-          //     id: story.id,
-          //     kids: story.kids,
-          //     score: story.score,
-          //     time: story.time,
-          //     title: story.title,
-          //     type: story.type,
-          //     url: story.url,
-          //   }
-          //   // console.log("new story", newStory)
-          //   storyDatas = [...storyDatas, newStory]
-          // }
-          // catch (error)
-          // {
-          //   console.log("fetching story data error", error);
-          // }
-
-        });
-        return storyDatas;
-      }),
-    onError: (error) => console.log(error)
-  });
-
-  const fetchAllStoryData = () => {
-    let localFetchAllArray: Promise<Response>[] = []
-
-    //collect all fetches into a single array
-    storiesIds.map((id: string) => {
-      const newFetch = fetch(fetchUrlBase + storyInfoUrlExtension + id + ".json", {
-        method: "GET",
-        headers: { "Content-Type": "application/json", },
-      })
-      localFetchAllArray = [...localFetchAllArray, newFetch]
-    })
-    setFetchAllArray(localFetchAllArray)
-  };
-
-
-
-
-
-  //-----------whenever the path changes, refetch the data-----------\\
-  useEffect(() => {
-    navItems.forEach((nav) => {
-      if (nav.localUrl == props.localUrl)
-        setFetchUrl(fetchUrlBase + nav.fetchUrl);
-    });
-    getStoriesIdsMutation();
-  }, [props.localUrl]);
-
-  useEffect(() => {
-    if (storiesIds) {
-      fetchAllStoryData();
-    } 
-  }, [storiesIds])
-
-  useEffect(() => {
-    const data = getStoryInfoMutation();
-    if (data !== undefined && data !== null) setAllStoryInfo(data);
-  }, [fetchAllArray])
-
-  useEffect(() => {
-    console.log("storyData", getStoryInfoData)
-    console.log("length", allStoryInfo.length)
-  }, [getStoryInfoData])
-
-
+ // Get full story data for each ID
+ const {
+   data: allStoryInfo,
+   isLoading: isLoadingStories,
+   isError: isErrorStories,
+ } = useQuery({
+   queryKey: ["storyData", storiesIds],
+   queryFn: async () => {
+     const fetchAllArray = storiesIds.slice(0, 10).map((id: number) =>
+       fetch(fetchUrlBase + storyInfoUrlExtension + id + ".json").then((res) => res.json())
+     );
+     return Promise.all(fetchAllArray);
+   },
+ });
 
 
 
   if (storiesIds === null) return <p>No data available</p>;
   if (storiesIds && storiesIds.error) return <p>Error: {storiesIds.error}</p>;
-  if (getStoryInfoStatus === "error") return <p>Error: {getStoryInfoError.name + " " + getStoryInfoError.message}</p>
-  if (!storiesIds || getStoryInfoStatus !== "success") return <p>Loading...</p>;
+  if (isErrorIds || isErrorStories) return <p>Error: data cannot be shown</p>
+  if (isLoadingIds || isLoadingStories) return <p>Loading...</p>;
 
   return (
     <>
       {allStoryInfo && allStoryInfo.length > 0 && (
         allStoryInfo.map((story: any) => (
-          // <Story  id={Number(id)}/>
-          <p>hi {story.id}</p>
+          <Story
+            by={story.by}
+            descendants={story.descendants}
+            id={story.id}
+            kids={story.kids}
+            score={story.score}
+            time={story.time}
+            title={story.title}
+            type={story.type}
+            url={story.url}
+          />
         )))
       }
     </>
--- hacker-news-application/src/Components/Story.tsx
+++ hacker-news-application/src/Components/Story.tsx
@@ -11,7 +11,19 @@ export interface StoryProps {
 }
 
 const Story: React.FC<StoryProps> = (props: StoryProps) => {
-  return props.id;
+  return (
+    <>
+      <p>{props.by}</p>
+      <p>{props.descendants}</p>
+      <p>{props.id}</p>
+      <p>{props.kids}</p>
+      <p>{props.score}</p>
+      <p>{props.time}</p>
+      <p>{props.title}</p>
+      <p>{props.type}</p>
+      <p>{props.url}</p>
+    </>
+  );
 }
 
 export default Story;
