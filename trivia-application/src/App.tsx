f9e7857d3f4a71827dd407fbb106fb8f66aa2454
lolyay270 <boyeje2@student.op.ac.nz>
2025-06-20 23:45:37 +1200

    feat(trivia): category fetching

--- trivia-application/src/App.tsx
+++ trivia-application/src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  let {
+    isLoading: isLoadingCategory,
+    isError: isErrorCategory,
+    data: categoryData,
+  } = useQuery({
+    queryKey: ["categoryData"],
+    queryFn: () =>
+      fetch("https://opentdb.com/api_category.php").then((res) => res.json()),
+  });
+  categoryData = categoryData.trivia_categories;
+
+  if (isLoadingCategory) return <p>Loading...</p>
+  if (isErrorCategory) return <p>Error: Cannot load data</p>
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {categoryData && (categoryData.map((cat: any) => (
+        <p>{cat.id}:  {cat.name}</p>
+      )))}
     </>
   )
 }
