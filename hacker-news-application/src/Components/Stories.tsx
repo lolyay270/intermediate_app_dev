e3b16c027c60879034520bbd8640418fa2c0e773
lolyay270 <boyeje2@student.op.ac.nz>
2025-06-20 21:24:35 +1200

    feat(hacker): story data validation

--- hacker-news-application/src/Components/Stories.tsx
+++ hacker-news-application/src/Components/Stories.tsx
@@ -10,6 +10,9 @@ import {
   CardTitle,
 } from "./ui/card"
 import { Button } from "./ui/button";
+import { useState } from "react";
+
+import { StoryProps } from "./Story";
 
 export const fetchUrlBase = "https://hacker-news.firebaseio.com/v0";
 export const storyInfoUrlExtension = "/item/";   // full url is .../v0/item/[id].json
@@ -17,6 +20,7 @@ export const storyInfoUrlExtension = "/item/";   // full url is .../v0/item/[id]
 const Stories: React.FC = () => {
   let fetchUrl: string = "";
   const localUrl = useLocation().pathname;
+  const [storyData, setStoryData] = useState<StoryProps[]>([])
 
   //set the fetch url based on the page were loading
   navItems.forEach(nav => {
@@ -43,12 +47,61 @@ const Stories: React.FC = () => {
   } = useQuery({
     queryKey: ["storyData", storiesIds],
     queryFn: async () => {
-      const fetchAllArray = storiesIds.slice(0, 25).map((id: number) =>
+      const fetchAllArray = storiesIds.map((id: number) =>
         fetch(fetchUrlBase + storyInfoUrlExtension + id + ".json").then((res) => res.json())
       );
       return Promise.all(fetchAllArray);
     },
   });
+
+  //Data validation (no missing values)
+  const checkStoryData = () => {
+    if (allStoryInfo) {
+      for (let i = 0; i < allStoryInfo.length; i++) {
+        let story: StoryProps = { //make essetially an empty object to add correct values to
+          by: "",
+          descendants: 0,
+          id: 0,
+          kids: [],
+          score: 0,
+          time: new Date(),
+          title: "",
+          type: "",
+          url: "",
+        };
+
+        if (allStoryInfo[i].by && typeof allStoryInfo[i].by === "string") story.by = allStoryInfo[i].by;
+        else continue;
+
+        if (allStoryInfo[i].descendants && typeof allStoryInfo[i].descendants === "number") story.descendants = allStoryInfo[i].descendants;
+        else continue;
+
+        if (allStoryInfo[i].id && typeof allStoryInfo[i].id === "number") story.id = allStoryInfo[i].id;
+        else continue;
+
+        if (allStoryInfo[i].kids && typeof allStoryInfo[i].kids === "object") story.kids = allStoryInfo[i].kids;
+        else continue;
+
+        if (allStoryInfo[i].score && typeof allStoryInfo[i].score === "number") story.score = allStoryInfo[i].score;
+        else continue;
+
+        if (allStoryInfo[i].time && typeof allStoryInfo[i].time === "number") story.time = new Date(allStoryInfo[i].time);
+        else continue;
+
+        if (allStoryInfo[i].title && typeof allStoryInfo[i].title === "string") story.title = allStoryInfo[i].title;
+        else continue;
+
+        if (allStoryInfo[i].type && typeof allStoryInfo[i].type === "string") story.type = allStoryInfo[i].type;
+        else continue;
+
+        story.url = fetchUrlBase + storyInfoUrlExtension + allStoryInfo[i].id + ".json";
+
+        setStoryData((storyData) => [...storyData, story])
+      }
+    }
+  }
+
+  if(storyData.length < 25) checkStoryData(); //get 25 stories that all have data
 
 
   if (storiesIds === null || allStoryInfo === null) return <p>No data available</p>;
@@ -57,8 +110,8 @@ const Stories: React.FC = () => {
 
   return (
     <div className="grid grid-cols-5 gap-4">
-      {allStoryInfo && allStoryInfo.length > 0 && (
-        allStoryInfo.map((story: any) => (
+      {storyData && storyData.length > 0 && (
+        storyData.map((story: any) => (
           <Card key={story.id} className="bg-blue-200">
             <CardHeader>
               <CardTitle>{story.title}</CardTitle>
