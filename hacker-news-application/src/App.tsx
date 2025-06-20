2de3b8b1f8b1d47faebda98bbb44e970bba46876
lolyay270 <boyeje2@student.op.ac.nz>
2025-06-20 21:29:59 +1200

    feat(hacker): default route is ask

--- hacker-news-application/src/App.tsx
+++ hacker-news-application/src/App.tsx
@@ -1,10 +1,16 @@
-import { useLocation } from "react-router";
+import { Navigate, useLocation } from "react-router";
 import Stories from "./Components/Stories";
 import Story from "./Components/Story";
 import Leaders from "./Components/Leaders";
 
 const App = () => {
   let currentRoute = useLocation();
+
+  //default route is ask stories
+  if (currentRoute.pathname === '/') {
+    return <Navigate to="/ask" replace />
+  }
+
   let isShowingStory = false;
 
   let storyId = currentRoute.pathname.slice(currentRoute.pathname.lastIndexOf("/") + 1); // +1 on index to remove "/"
