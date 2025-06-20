3672236fe7d3b23e3d53d6d7ce9598d9f474b281
lolyay270 <boyeje2@student.op.ac.nz>
2025-06-20 15:57:40 +1200

    feat(hacker): basic leaders search

--- hacker-news-application/src/Components/Leaders.tsx
+++ hacker-news-application/src/Components/Leaders.tsx
@@ -1,4 +1,7 @@
-export const leadersList = [
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const leadersList = [
   "tptacek",
   "jacquesm",
   "ingve",
@@ -12,7 +15,56 @@ export const leadersList = [
 ];
 
 const Leaders: React.FC = () => {
-  return <p>{leadersList}</p>;
-};
+  const leadersForm = useForm();
+  const [searchedNames, setSearchedNames] = useState<string[]>([]);
+
+
+  const handleLeadersSubmit = (values: any) => {
+    setSearchedNames([]);
+    console.log(values.name)
+    leadersList.map((leader) => {
+      if (leader.includes(values.name)) {
+        setSearchedNames(searchedNames => [...searchedNames, leader])
+      } 
+    })
+    console.log("searchedNames", searchedNames)
+  }
+
+console.log(leadersForm.formState.isSubmitted)
+
+  return (
+    <>
+      <p>Search for a username within the top users</p>
+      <p>To view all top 10, leave the text field empty</p>
+      <br/>
+
+      <form onSubmit={leadersForm.handleSubmit(handleLeadersSubmit)}>
+        <label htmlFor="name">Username</label>
+        <input type="text" id="name" {...leadersForm.register("name")} />
+
+        <button type="submit">Search</button>
+      </form>
+
+      <br/>
+      <br/>
+
+      <p>Search results:</p>
+      {searchedNames.length > 0 ? (
+        <>
+          {searchedNames.map((username) => (
+            <p>{username}</p>
+          ))}
+        </>
+
+      ) : (
+        leadersForm.formState.isSubmitted ? (
+          <p>No usernames match the search</p>
+        ) : (
+          <p>Press the "Search" button to find a username</p>
+        )
+      )}
+    </>
+  )
+}
 
 export default Leaders;
